86ee94087cb17f9198d26c13abccb89149d03841
maxwellbytes <brownmh3@appstate.edu>
2026-07-11 12:25:45 -0400

    starting react refactor

---
+++ portfolio/src/app/page copy.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  return (
+    <h1 className="bg-amber-600">Test Page!</h1>
+  );
+}
---
+++ portfolio/src/app/task_bar.tsx
@@ -0,0 +1,9 @@
+export default function TaskBar() {
+  return (
+    <>
+      <nav className="bg-amber-600 h-[6vh]">
+        <button className="bg-sky-700 h-full w-32">Start</button>
+      </nav>
+    </>
+  );
+}
